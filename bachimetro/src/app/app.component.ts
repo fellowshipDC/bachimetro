2da05af03ada3f81e435205e9938ac898292333e
Jorge Andrade <jorandradefig@gmail.com>
2017-12-14 15:56:02 -0600

    Add map with tweets

--- bachimetro/src/app/app.component.ts
+++ bachimetro/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Response, Headers} from '@angular/http';
-import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -24,7 +23,7 @@ export class AppComponent {
       {
         params : {
           token_type : "bearer",
-          access_token : "AAAAAAAAAAAAAAAAAAAAANxN3QAAAAAASPfkYaEsbzos4GzCvdR9tLQV%2BXU%3DZ9rsMl6Fy4U9VvpauaMFDKBLr2ViBHy2QFQxlJuVEgw0ZAZYR1"  
+          access_token : "AAAAAAAAAAAAAAAAAAAAANxN3QAAAAAASPfkYaEsbzos4GzCvdR9tLQV%2BXU%3DZ9rsMl6Fy4U9VvpauaMFDKBLr2ViBHy2QFQxlJuVEgw0ZAZYR1"
         },
       }
     )
@@ -37,7 +36,7 @@ export class AppComponent {
 */
 //  .get('https://api.twitter.com/1.1/search/tweets.json?q=%23bachimetro')
 
-  
+
 
   ngOnInit(){
 
--- bachimetro/src/app/tweets/tweets.component.ts
+++ bachimetro/src/app/tweets/tweets.component.ts
@@ -1,13 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Http, HttpModule, Response } from '@angular/http';
-import * as io from 'socket.io-client';
-import { Observable } from 'rxjs/Observable';
-import * as Rx from 'rxjs/Rx';
 import { HttpClient } from '@angular/common/http';
 
-declare const google : any;
-declare const map: any;
-declare var heatmap : any;
+declare var $: any;
+declare var google: any;
 
 
 @Component({
@@ -25,81 +20,55 @@ export class TweetsComponent implements OnInit {
   constructor(private http: HttpClient) {
   }
 
+  initMap() {
+    var cdmx = { lat: 19.4286973, lng: -99.156051 };
+    var light_grey_style = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
+    var options = {
+      zoom: 5,
+      center: cdmx,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        position: google.maps.ControlPosition.LEFT_BOTTOM
+      },
+      styles: light_grey_style
+    };
+    var map = new google.maps.Map(document.getElementById('map'), options);
+
+    return map;
+  }
+
+  addMarker(map, lat, lng) {
+    var location = { lat: lat, lng: lng };
+    var image = "css/marker_bachimetro.png";
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      icon: image
+    });
+  }
+
   ngOnInit() {
 
     this.http
     .get(this.url)
     .subscribe(
-      res => {
+      (res: any) => {
         console.log(res, 'ok');
+        let map = this.initMap();
+
+        res.body.statuses.map(status => {
+          if (status.geo !== null) {
+            this.addMarker(map, status.geo.lng, status.geo.lat);
+          }
+        });
+
       },
       err => {
         console.log('error');
       }
     );
 
-    //Setup Google Map
-    function initMap() {
-        //Setup Google Map
-  var light_grey_style = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
-  var myOptions = {
-    zoom: 5,
-    center: {lat: 19.432608, lng: -99.133209},
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    mapTypeControl: true,
-    mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-      position: google.maps.ControlPosition.LEFT_BOTTOM
-    },
-    styles: light_grey_style
-  };
-  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-
-  //Setup heat map and link to Twitter array we will append data to
-
-  var liveTweets = new google.maps.MVCArray();
-  heatmap = new google.maps.visualization.HeatmapLayer({
-    data: liveTweets,
-    radius: 25
-  });
-  heatmap.setMap(null); //Set to null to remove heatmap
-
-  if(io !== undefined) {
-    // Storage for WebSocket connections
-    var socket = io.connect('/');
-
-    // This listens on the "twitter-steam" channel and data is
-    // received everytime a new tweet is receieved.
-    socket.on('twitter-stream', function (data) {
-
-      //Add tweet to the heat map array.
-      var tweetLocation = new google.maps.LatLng(data.lng,data.lat);
-      liveTweets.push(tweetLocation);
-
-      //Flash a dot onto the map quickly
-      var image = "css/marker_bachimetro.png";
-      var marker = new google.maps.Marker({
-        position: tweetLocation,
-        map: map,
-        icon: image
-      });
-      setTimeout(function(){
-        marker.setMap(map);
-      },60000);
-
-    });
-
-    // Listens for a success response from the server to
-    // say the connection was successful.
-    socket.on("connected", function(r) {
-
-      //Now that we are connected to the server let's tell
-      //the server we are ready to start receiving tweets.
-      socket.emit("start tweets");
-    });
-  }
-    }
-
-    initMap();
   }
 }
