12f481f711128afc37d3943a63c79185ce798914
Jorge Andrade <jorandradefig@gmail.com>
2017-12-14 14:51:26 -0600

    Add tweets endpoint

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ var cors = require('cors');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var tweets = require('./routes/tweets');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/tweets', tweets);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
