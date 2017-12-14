bd7729aa7f302140380e4ee46c9e67b56d8e8758
Jose Luis <puenterodriguezjose@gmail.com>
2017-12-14 11:52:17 -0600

    sockets

--- bachimetro/server.js
+++ bachimetro/server.js
@@ -1,31 +1,86 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const http = require('http');
-const app = express();
+//Setup web server and socket
+var twitter = require('twitter'),
+    express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
 
-// API file for interacting with MongoDB
-const api = require('./server/routes/api');
+//Setup twitter stream api
+var twit = new twitter({
+  consumer_key: 'yAXoXCoZvZZUmUWs0p2GTNH2s',
+  consumer_secret: 'vp0y26xc1HhWVmIsFTAzEDAoEGzhtzVUsJw3hGY2xJ4ohbzKrp',
+  access_token_key: '1054591028-Ne272TsQw6lUtw6VLJt2CY0zic21YLr7MUrCRPx',
+  access_token_secret: '0usi86t3E5wZH1WaZ2nULZ62kfmTrZzAAUe5iEIlczTGF'
+}),
+stream = null;
 
-// Parsers
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+//Use the default port (for beanstalk) or default to 8081 locally
+server.listen(process.env.PORT || 4200);
 
-// Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist')));
+//Setup rotuing for app
+app.use(express.static(__dirname + '/public'));
 
-// API location
-app.use('/api', api);
+//Create web sockets connection.
+io.sockets.on('connection', function (socket) {
 
-// Send all other requests to the Angular app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  socket.on("start tweets", function() {
+
+    if(stream === null) {
+      //Connect to twitter stream passing in filter for entire world.
+      twit.stream('statuses/filter', {'locations':'-118.652,13.411,-85.428,31.353', track : 'meade'}, function(stream) {
+          stream.on('data', function(data) {
+              // Does the JSON result have coordinates
+              if (data.coordinates){
+                if (data.coordinates !== null){
+                  //If so then build up some nice json and send out to web sockets
+                  var outputPoint = {"lat": data.coordinates.coordinates[0],"lng": data.coordinates.coordinates[1]};
+
+                  socket.broadcast.emit("twitter-stream", outputPoint);
+
+                  //Send out to web sockets channel.
+                  socket.emit('twitter-stream', outputPoint);
+                }
+                else if(data.place){
+                  if(data.place.bounding_box === 'Polygon'){
+                    // Calculate the center of the bounding box for the tweet
+                    var coord, _i, _len;
+                    var centerLat = 0;
+                    var centerLng = 0;
+
+                    for (_i = 0, _len = coords.length; _i < _len; _i++) {
+                      coord = coords[_i];
+                      centerLat += coord[0];
+                      centerLng += coord[1];
+                    }
+                    centerLat = centerLat / coords.length;
+                    centerLng = centerLng / coords.length;
+
+                    // Build json object and broadcast it
+                    var outputPoint = {"lat": centerLat,"lng": centerLng};
+                    socket.broadcast.emit("twitter-stream", outputPoint);
+
+                  }
+                }
+              }
+              stream.on('limit', function(limitMessage) {
+                return console.log(limitMessage);
+              });
+
+              stream.on('warning', function(warning) {
+                return console.log(warning);
+              });
+
+              stream.on('disconnect', function(disconnectMessage) {
+                return console.log(disconnectMessage);
+              });
+          });
+      });
+    }
+  });
+
+    // Emits signal to the client telling them that the
+    // they are connected and can start receiving Tweets
+    socket.emit("connected");
 });
 
-//Set Port
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-const server = http.createServer(app);
-
-server.listen(port, () => console.log(`Running on localhost:${port}`));
